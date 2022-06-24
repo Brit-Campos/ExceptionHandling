9ac9003131ac3dff5b862df3761903a29038e014
Brit <Brit@DESKTOP-BFORP22>
2022-06-24 16:18:50 -0700

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,38 @@
+﻿// exception = errors that occur during execution
+
+//      try     = try some code that is considered "dangerous"
+//      catch   = catches and handles exceptions when they occur
+//      finally = always executes regardless if exception is caught or not
+
+try
+{
+    double x;
+    double y;
+    double result;
+
+    Console.Write("Enter number 1: ");
+    x = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter number 2: ");
+    y = Convert.ToInt32(Console.ReadLine());
+
+    result = x / y;
+
+    Console.WriteLine("Result: " + result);
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Enter ONLY numbers.");
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("You can't divide by zero.");
+}
+catch (Exception e)
+{
+    Console.WriteLine("Something went wrong.");
+}
+finally
+{
+    Console.WriteLine("Thanks for visiting.");
+}
